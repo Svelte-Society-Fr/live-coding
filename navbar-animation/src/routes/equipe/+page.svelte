b484e2e89fb29ddb510f310aca7bc70b4a03765c
Bekiboo <julien.connault@gmail.com>
2023-06-23 16:18:02 +0300

    Add navbar animation

---
+++ navbar-animation/src/routes/equipe/+page.svelte
@@ -0,0 +1,45 @@
+<!-- Team.svelte -->
+<main>
+	<h1>Our Team</h1>
+
+	<ul>
+		<li>
+			<h2>John Doe</h2>
+			<p>CEO</p>
+		</li>
+		<li>
+			<h2>Jane Smith</h2>
+			<p>COO</p>
+		</li>
+		<li>
+			<h2>David Johnson</h2>
+			<p>CTO</p>
+		</li>
+	</ul>
+</main>
+
+<style>
+	main {
+		max-width: 600px;
+		margin: 0 auto;
+		padding: 20px;
+		font-family: Arial, sans-serif;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	ul {
+		list-style-type: none;
+		padding: 0;
+	}
+
+	li {
+		margin-bottom: 20px;
+	}
+
+	h2 {
+		margin-bottom: 5px;
+	}
+</style>
---
+++ navbar-animation/src/routes/rejoindre/+page.svelte
@@ -0,0 +1,61 @@
+<!-- Donate.svelte -->
+<script>
+	let donationAmount = 0;
+
+	function handleDonate() {
+		// Logic to process the donation goes here
+		// You can use the donationAmount variable to access the selected amount
+		// and send it to your backend for further processing
+		console.log('Donation amount:', donationAmount);
+	}
+</script>
+
+<main>
+	<h1>Donate</h1>
+
+	<p>Select an amount to donate:</p>
+	<div>
+		<label>
+			<input type="radio" bind:group={donationAmount} value="10" />
+			$10
+		</label>
+	</div>
+	<div>
+		<label>
+			<input type="radio" bind:group={donationAmount} value="20" />
+			$20
+		</label>
+	</div>
+	<div>
+		<label>
+			<input type="radio" bind:group={donationAmount} value="50" />
+			$50
+		</label>
+	</div>
+
+	<button on:click={handleDonate}>Donate</button>
+</main>
+
+<style>
+	main {
+		max-width: 400px;
+		margin: 0 auto;
+		padding: 20px;
+		font-family: Arial, sans-serif;
+	}
+
+	h1 {
+		text-align: center;
+	}
+
+	div {
+		margin-bottom: 10px;
+	}
+
+	button {
+		display: block;
+		margin: 20px auto;
+		padding: 10px 20px;
+		font-size: 16px;
+	}
+</style>
